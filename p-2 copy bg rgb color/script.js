23b8674f6c3f97a5caaccd5aef08bb81c1142564
Shahriar Tamim <127070914+shahriartamim2@users.noreply.github.com>
2024-01-18 19:19:49 +0600

    Add files via upload

---
+++ p-2 copy bg rgb color/script.js
@@ -0,0 +1,19 @@
+const x = document.querySelector(".btn");
+x.addEventListener('click', myfunc);
+
+function myfunc(){
+    const bgcolor = randColor();
+    let y = document.querySelector(".cont");
+    y.style.backgroundColor = bgcolor;
+    const z = document.querySelector(".inpt")
+    z.value = randColor();
+}
+
+function randColor(){
+    let a = Math.floor(0 +(1 - Math.random())*255);
+    let b = Math.floor(0 +(1 - Math.random())*255);
+    let c = Math.floor(0 +(1 - Math.random())*255);
+
+    return `rgb(${a},${b},${c})`;
+}
+
